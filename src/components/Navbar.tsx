cef76386963daefeb08523650d76d6cee2ea9bca
ankur4work <ankur4worksabai@gmail.com>
2026-05-01 01:52:40 +0530

    Logo fills navbar height (68px navbar, 64px elsewhere)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -49,7 +49,7 @@ export default function Navbar() {
 
           {/* Logo */}
           <Link href="/" className="flex items-center">
-            <Image src="/logo.png" alt="SteelLine Logistics" width={140} height={56} className="h-[40px] w-auto object-contain" priority />
+            <Image src="/logo.png" alt="SteelLine Logistics" width={140} height={56} className="h-[68px] w-auto object-contain" priority />
           </Link>
 
           {/* Desktop Nav */}
